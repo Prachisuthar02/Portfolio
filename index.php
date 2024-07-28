f75f4c8633b11589228449997c328481ef0a7d1b
Prachisuthar02 <sutharprachi20@gmail.com>
2024-07-28 16:32:45 +0530

    upload files to git

---
+++ index.php
@@ -0,0 +1,423 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyPortfolio</title>
+
+    <!-- font-awsome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- aos css link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+
+
+
+<!-- header section start-->
+  <header class="header">
+    
+    <div id="menu-btn" class="fas fa-bars"></div>
+    
+    <a href="#home" class="logo">Portfolio</a> 
+
+    <nav class="navbar">
+        
+    <a href="#home" class="active">Home</a>
+    <a href="#about">about</a>
+    <a href="#skills">skills</a>
+    <a href="#portfolio">portfolio</a>
+    <a href="#contact">contact</a>
+    </nav>
+
+    <div class="follow">
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        <a href="#" class="fab fa-github"></a>
+    </div>
+
+  </header>
+<!-- header section end -->
+
+
+<!-- Home Section Start -->
+<section class="home" id="home">
+    <div class="image" data-aos="fade-right">
+        <img src="media/img2.webp" alt="">
+    </div>
+
+    <div class="content">
+        <h3 data-aos="fade-up">Hi, i am prachi suthar</h3>
+        <span data-aos="fade-up">frontend web developer</span>
+        <p data-aos="fade-up">I'm passionate about creating & engaging user-friendly websites as a Frontend Web Developer. Eager to learn and grow in the world of web development.</p>
+        <a data-aos="fade-up" href="#about" class="btn">about me</a>
+    </div>
+</section>
+<!-- Home Section end -->
+
+
+<!-- about section start -->
+    <section class="about" id="about">
+
+      <h1 class="heading" data-aos="fade-up"><span>Biography</span></h1>
+      <div class="biography">
+        <p data-aos="fade-up">Hey, I'm Prachi suthar, a Computer Science Engineering student diving into the world of frontend web development with expertise in HTML, CSS, and JavaScript. Additionally, I possess a well better understanding of PHP Language and Database Management System(DBMS) such as MySql and MongoDB. Eager to apply my skills and creating user-friendly and visually apealing websites. I'm excited to explore new opportunities and grow in the field of web Development. </p>
+      
+        <div class="bio">
+            <h3 data-aos="zoom-in"><span>name : </span>Prachi Suthar</h3>
+            <h3 data-aos="zoom-in"><span>email : </span>prachisuthar@gmail.com</h3>
+            <h3 data-aos="zoom-in"><span>address : </span>Rajasthan, India</h3>
+            <h3 data-aos="zoom-in"><span>Phone : </span>+91 123-456-7890</h3>
+            <h3 data-aos="zoom-in"><span>Experience : </span>Fresher</h3>
+        </div>
+    
+        <a href="#" class="btn" data-aos="fade-up">download CV</a>
+    </div>
+
+    </section>
+<!-- about section end -->
+
+    <!-- skills section start -->
+    <section class="skills section" id="skills">
+    <h2 class="heading" data-aos="fade-up"><span>Skills</span></h2>
+    <span class="section_subtitle" data-aos="fade-up">My technical level</span>
+
+    <div class="skills_container container grid">
+        <div>
+
+
+            <!-- skills 1 -->
+
+            <div class="skills_content Skills_open">
+                <div class="skills_header">
+                   
+                    <div>
+                        <!-- <div class="box"> -->
+                        <h1 class="skills_title1 boxes">Programming Languages</h1>
+                    <!-- </div> -->
+
+                <div class="skills_list grid">
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">C </h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">C++</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">HTML</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">CSS</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">JavaScript</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                       
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">PHP</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">DBMS</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+</div>
+</div>
+
+            <!-- skills 2 -->
+            <div class="skills_content Skills_open" >
+                <div class="skills_header">
+            
+                    <div>
+                        <h1 class="skills_title boxes">Plateforms & Tools</h1>
+                    </div>
+
+                </div>
+
+                <div class="skills_list grid">
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">MySql</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">MongoDB</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">GitHub</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">VSCode</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">Microsoft Tools</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+                  
+
+            <!-- skills 3 -->
+            <div class="skills_content Skills_open">
+                <div class="skills_header">
+                  
+                    <div>
+                        <h1 class="skills_title boxes">Professional Skills</h1>
+                    </div>
+         
+                </div>
+
+                <div class="skills_list grid">
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">Effective Communication</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">Teamwork & Leadership</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">Problem Solving</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                    <div class="skills_data">
+                        <div class="skills_title">
+                            <h3 class="skills_name">Time Management</h3>
+                        </div>
+                        <div class="skills_bar">
+                            <span class="skills_percentage"></span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+    
+    
+<!--skills section end -->
+
+
+<!-- Portfolio section start -->
+<section class="portfolio" id="portfolio">
+
+     <h1 class="heading"><span>Portfolio</span></h1>
+
+    <div class="box-container">
+       
+    <div class="box" data-aos="zoom-in">
+        <img src="media/flowerweb.png" alt="">
+        <h3>Web Development</h3>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <img src="media/portfolio imgsc2.png" alt="">
+        <h3>Web Development</h3>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <img src="media/portfolio imgsc3.png" alt="">
+        <h3>Web Development</h3>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <img src="media/portfolio imgsc4.png" alt="">
+        <h3>Web Development</h3>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <img src="media/portfolio imgsc5.png" alt="">
+        <h3>Web Development</h3>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <img src="media/portfolio imgsc6.png" alt="">
+        <h3>Web Development</h3>
+    </div>
+
+
+    </div>
+
+</section>
+<!-- Portfolio section End -->
+
+
+
+<!-- contact section start -->
+<section class="contact" id="contact">
+    <h1 class="heading"><span>contact me</span></h1>
+
+    <form action=""  method="post">
+          <div class="flex">
+            <input type="text" name="name" placeholder="enter your name" class="box" required>
+            <input  type="email" name="email" placeholder="enter your email" class="box" required>
+            </div>
+            <input  type="number" min="0" name="number" placeholder="enter your number" class="box" required>
+            <textarea name="meassage" class="box" required placeholder="enter your message" cols="30" rows="10"></textarea>
+            <input type="submit"  value="send message" name="send" class="btn"> 
+    </form>
+
+    <?php
+
+$host = "localhost";
+$username = "root";
+$password = null;
+
+ $conn= new PDO("mysql:host=$host;dbname=portfolio", $username, $password);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+     
+
+ if($_POST){
+    $name= $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $message = $_POST['meassage'];
+
+  $sqlQuery = "INSERT INTO contact VALUES(null,'$name', '$email', '$number', '$message')";
+
+  $insertData = $conn->prepare($sqlQuery);
+  $insertData->execute();
+ 
+ }
+?>
+
+
+    <div class="box-container">
+        <div class="box">
+            <i class="fas fa-phone"></i>
+            <h3>phone</h3>
+            <p>+123-456-7890</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-envelope"></i>
+            <h3>email</h3>
+            <p>prachisuthar@gmail.com</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-map-marker-alt"></i>
+            <h3>address</h3>
+            <p>Rajasthan, India - 123456</p>
+        </div>
+    </div>
+
+</section>
+
+<!-- contact section end -->
+
+<div class="credit"> Prachi Suthar @ <?php echo date('Y'); ?> <span>Frontend Web Developer</span></div>
+
+
+<script src="script.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
+<script>
+
+    AOS.init({
+        duration: 800, 
+        delay: 300
+    });
+
+</script>
+
+
+</body>
+</html>
