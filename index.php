924486feb83e6ae48871af163033254821133159
Prachisuthar02 <sutharprachi20@gmail.com>
2024-07-28 16:38:43 +0530

    changes made in index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
     <div class="follow">
         <a href="#" class="fab fa-instagram"></a>
         <a href="#" class="fab fa-linkedin"></a>
-        <a href="#" class="fab fa-github"></a>
+        <a href="https://github.com/Prachisuthar02" class="fab fa-github"></a>
     </div>
 
   </header>
